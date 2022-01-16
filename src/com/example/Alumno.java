42fb39f9d3107a8f52ec5dab4e393a3de2a3b9bf
tomasdeambrosi <tomasdeambrosi@gmail.com>
2022-01-16 13:51:35 -0300

    Add files via upload

---
+++ src/com/example/Alumno.java
@@ -0,0 +1,24 @@
+package com.example;
+
+public class Alumno {
+
+    public static void main(String[] args) {
+
+        String nombreCompleto = "Carlos Menem";
+        int edad = 53;
+        long telefono = 458998768;
+        char division = 'A';
+        double promedio = 7.88d;
+        boolean aprobado = true;
+
+        System.out.println(nombreCompleto);
+        System.out.println(edad);
+        System.out.println(telefono);
+        System.out.println(division);
+        System.out.println(promedio);
+        System.out.println(aprobado);
+
+    }
+
+
+}
